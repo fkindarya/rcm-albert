634c91e80d5378ce9d32d77b19490188fed54e25
Fabyan Kindarya <fkindarya@gmail.com>
2023-01-09 15:08:32 +0700

    Create Read Delete Done

--- routes/flow.route.js
+++ routes/flow.route.js
@@ -2,33 +2,180 @@ const express = require('express')
 const flowRouter = express.Router()
 const { db } = require('./firebase')
 
-flowRouter.post('/data', async(req, res) => {
+flowRouter.post('/add-data', async(req, res) => {
     const data = await req.body
     
     const id = '_' + new Date().getTime()
     const json = {
         id: id,
         mtbf: data.mtbf,
-        reliability: data.reliability,
-        history: []
+        reliability: data.reliability
     }
-    console.log(json)
     
-    const response = await db.collection('flows').doc(id).set(json)
-    
+    const flowsDb = db.collection('flows').doc(id)
+    const response = await flowsDb.set(json)
     res.send(response)
 })
 
-flowRouter.get('/data', async (req, res) => {
+flowRouter.post('/:id/add-history', async (req, res) => {
+    const data = await req.body
+    
+    const id = '_' + new Date().getTime()
+    const json = {
+        id: id,
+        name: data.name
+    }
+
+    const flowsDb = db.collection('flows').doc(req.params.id).collection('history').doc(id)
+    const response = await flowsDb.set(json)
+    res.send(response)
+})
+
+flowRouter.post('/:id/:idHistory/add-data', async (req, res) => {
+    const data = await req.body
+
+    const id = '_' + new Date().getTime()
+    const json = {
+        id: id,
+        duration: data.duration,
+        time: data.time,
+        status: data.status
+    }
+
+    const flowsDb = db.collection('flows').doc(req.params.id).collection('history').doc(req.params.idHistory).collection('data').doc(id)
+    const response = await flowsDb.set(json)
+    res.send(response)
+})
+
+flowRouter.get('/all', async (req, res) => {
     const flowsDb = db.collection('flows')
     const response = await flowsDb.get()
 
     let dataArray = []
-    response.forEach(data => {
-        dataArray.push(data.data())
+    response.forEach(doc => {
+        dataArray.push(doc.data())
     })
 
     res.send(dataArray)
 })
+
+flowRouter.get('/:id', async (req, res) => {
+    const flowsDb = db.collection('flows').doc(req.params.id)
+    const responseFlow = await flowsDb.get()
+    
+    const flowHistoriesDb = flowsDb.collection('history')
+    const responseFlowHistories = await flowHistoriesDb.get()
+
+    let arrayHistory = []
+    responseFlowHistories.forEach(doc => {
+        arrayHistory.push(doc.data())
+    })
+
+    res.send({
+        flows: responseFlow.data(),
+        flowHistories: arrayHistory
+    })
+
+    // res.send(responseFlow.data())
+    // const flowsHistoryDb = flowsDb.collection('history')
+    // const responseFlowHistory = await flowsHistoryDb.get()
+    // let arrayHistory = []
+    // responseFlowHistory.forEach(doc => {
+    //     arrayHistory.push(doc.data())
+    // })
+
+    // let hayuk = Object.assign(responseFlow.data(), arrayHistory)
+    // res.send(hayuk)
+})
+
+flowRouter.get('/:id/:idHistory', async (req, res) => {
+    const flowsDb = db.collection('flows').doc(req.params.id)
+    const responseFlow = await flowsDb.get()
+
+    const flowHistoriesDb = flowsDb.collection('history').doc(req.params.idHistory)
+    const responseFlowHistory = await flowHistoriesDb.get()
+
+    const flowHistoryDatasDb = flowHistoriesDb.collection('data')
+    const responseFlowHistoryData = await flowHistoryDatasDb.get()
+
+    let arrayHistoryData = []
+    responseFlowHistoryData.forEach(doc => {
+        arrayHistoryData.push(doc.data())
+    })
+
+    res.send({
+        flows: responseFlow.data(),
+        flowHistories: responseFlowHistory.data(),
+        flowHistoryData: arrayHistoryData
+    })
+})
+
+flowRouter.delete('/:id/:idHistory/:idData', async (req, res) => {
+    const flowsDb = db.collection('flows').doc(req.params.id)
+    const flowHistoriesDb = flowsDb.collection('history').doc(req.params.idHistory)
+    const flowHistoryDatasDb = flowHistoriesDb.collection('data').doc(req.params.idData)
+
+    const response = await flowHistoryDatasDb.delete()
+    res.send(response)
+})
+
+// flowRouter.post('/data/:id/history', async (req, res) => {
+//     const data = await req.body
+
+//     const id = '_' + new Date().getTime()
+//     const json = {
+//         id: id,
+//         name: data.name,
+//         flowId: "/flows/" + req.params.id
+//     }
+
+//     const flowsDb = db.collection('flow_histories').doc(id)
+//     const response = await flowsDb.set(json)
+//     res.send(response)
+// })
+
+// flowRouter.post('/data/:idFlow/history/:id', async (req, res) => {
+//     const data = await req.body
+
+//     const id = '_' + new Date().getTime()
+//     const json = {
+//         id: id,
+//         duration: data.duration,
+//         time: data.time,
+//         status: data.status,
+//         flowHistoryId: req.params.id,
+//         flowId: req.params.idFlow
+//     }
+
+//     const flowsDb = db.collection('flow_history_datas').doc(id)
+//     const response = await flowsDb.set(json)
+//     res.send(response)
+// })
+
+// flowRouter.get('/data/:id', async (req, res) => {
+//     const { id } = req.params
+//     const flowsDb = db.collection('flows').doc(id)
+//     const responseFlow = await flowsDb.get()
+
+//     const flowHistoriesDb = db.collection('flow_histories')
+//     const responseFlowHistory = await flowHistoriesDb.where('flowId', '==', id).get()
+//     let historyArray = []
+//     responseFlowHistory.forEach(doc => {
+//         historyArray.push(doc.data())
+//     })
+
+//     const flowHistoryDataDb = db.collection('flow_history_datas')
+//     const responseFlowHistoryData = await flowHistoryDataDb.where('flowId', '==', id).get()
+//     let historyDataArray = []
+//     responseFlowHistoryData.forEach(doc => {
+//         historyDataArray.push(doc.data())
+//     })
+
+//     res.send({
+//         flow: responseFlow.data(), 
+//         flowHistory: historyArray,
+//         flowHistoryData: historyDataArray
+//     })
+// })
 
 module.exports = flowRouter
--- routes/vibration.route.js
+++ routes/vibration.route.js
@@ -2,33 +2,110 @@ const express = require('express')
 const vibrationRouter = express.Router()
 const { db } = require('./firebase')
 
-vibrationRouter.post('/data', async(req, res) => {
+vibrationRouter.post('/add-data', async(req, res) => {
     const data = await req.body
     
     const id = '_' + new Date().getTime()
     const json = {
         id: id,
         mtbf: data.mtbf,
-        reliability: data.reliability,
-        history: []
+        reliability: data.reliability
     }
-    console.log(json)
     
-    const response = await db.collection('vibrations').doc(id).set(json)
-    
+    const vibrationsDb = db.collection('vibrations').doc(id)
+    const response = await vibrationsDb.set(json)
     res.send(response)
 })
 
-vibrationRouter.get('/data', async (req, res) => {
+vibrationRouter.post('/:id/add-history', async (req, res) => {
+    const data = await req.body
+    
+    const id = '_' + new Date().getTime()
+    const json = {
+        id: id,
+        name: data.name
+    }
+
+    const vibrationsDb = db.collection('vibrations').doc(req.params.id).collection('history').doc(id)
+    const response = await vibrationsDb.set(json)
+    res.send(response)
+})
+
+vibrationRouter.post('/:id/:idHistory/add-data', async (req, res) => {
+    const data = await req.body
+
+    const id = '_' + new Date().getTime()
+    const json = {
+        id: id,
+        duration: data.duration,
+        time: data.time,
+        status: data.status
+    }
+
+    const vibrationsDb = db.collection('vibrations').doc(req.params.id).collection('history').doc(req.params.idHistory).collection('data').doc(id)
+    const response = await vibrationsDb.set(json)
+    res.send(response)
+})
+
+vibrationRouter.get('/all', async (req, res) => {
     const vibrationsDb = db.collection('vibrations')
     const response = await vibrationsDb.get()
 
     let dataArray = []
-    response.forEach(data => {
-        dataArray.push(data.data())
+    response.forEach(doc => {
+        dataArray.push(doc.data())
     })
 
     res.send(dataArray)
+})
+
+vibrationRouter.get('/:id', async (req, res) => {
+    const vibrationsDb = db.collection('vibrations').doc(req.params.id)
+    const responsevibration = await vibrationsDb.get()
+    
+    const vibrationHistoriesDb = vibrationsDb.collection('history')
+    const responsevibrationHistories = await vibrationHistoriesDb.get()
+
+    let arrayHistory = []
+    responsevibrationHistories.forEach(doc => {
+        arrayHistory.push(doc.data())
+    })
+
+    res.send({
+        vibrations: responsevibration.data(),
+        vibrationHistories: arrayHistory
+    })
+})
+
+vibrationRouter.get('/:id/:idHistory', async (req, res) => {
+    const vibrationsDb = db.collection('vibrations').doc(req.params.id)
+    const responsevibration = await vibrationsDb.get()
+
+    const vibrationHistoriesDb = vibrationsDb.collection('history').doc(req.params.idHistory)
+    const responsevibrationHistory = await vibrationHistoriesDb.get()
+
+    const vibrationHistoryDatasDb = vibrationHistoriesDb.collection('data')
+    const responsevibrationHistoryData = await vibrationHistoryDatasDb.get()
+
+    let arrayHistoryData = []
+    responsevibrationHistoryData.forEach(doc => {
+        arrayHistoryData.push(doc.data())
+    })
+
+    res.send({
+        vibrations: responsevibration.data(),
+        vibrationHistories: responsevibrationHistory.data(),
+        vibrationHistoryData: arrayHistoryData
+    })
+})
+
+vibrationRouter.delete('/:id/:idHistory/:idData', async (req, res) => {
+    const vibrationsDb = db.collection('vibrations').doc(req.params.id)
+    const vibrationHistoriesDb = vibrationsDb.collection('history').doc(req.params.idHistory)
+    const vibrationHistoryDatasDb = vibrationHistoriesDb.collection('data').doc(req.params.idData)
+
+    const response = await vibrationHistoryDatasDb.delete()
+    res.send(response)
 })
 
 module.exports = vibrationRouter
